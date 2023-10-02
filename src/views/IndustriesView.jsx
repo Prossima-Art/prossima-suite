a77b0646ed20671239183534cd1f4a101a4369c8
alanny-design <alannylucena@gmail.com>
2023-10-02 14:43:00 -0300

    docs: update industries page

--- src/views/IndustriesView.jsx
+++ src/views/IndustriesView.jsx
@@ -9,23 +9,22 @@ import Navbar from "../sections/navbar/navbar";
 
 
 function IndustriesView() {
-    const heroText = "Productivity and Efficiency tools for all companies."
-    const videoUrl = "https://s3-figma-videos-production-sig.figma.com/video/835212680197847456/TEAM/961f/0f7b/-419e-460b-87ae-f4cab050476e?Expires=1695600000&Signature=mqfK8E5sPt8I32L19Ll4sr-Crgg3kvW1XKHQoJ-SrttW1RNdOk9a~SCSUN9Zj8zHpYZROXnQFFi3pFub9UEN0onI4CU1OBnAYkM7xUiQNNLKj0jw80QqrOLLc3AyObPZm4vpx68eU6QD6gczVMO~Gtbvug723oHTdInwbnKNa5HiXJNtWnORDYBnFGEWmmaD5GyTU0nraqJm3OgilQwSH541zvD5haqei~X0zg9MAygKe88y3bKGZSP8FcpDEEJmUzBUPoO7S1~TdbpfG-dIsSBzHI-g-b-BmT0CMefN86cCGGMWAOQzDDb4zvBFjX0YDEmyEEZ88DHJytdEZ9fZ-A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
     return (
         <>
             <Navbar />
             <Hero />
-            <Ecommerce/>
+            <Ecommerce id="Ecommerce"
+            />
             <InfoTextEcommerce/>
-            <Finance/>
+            <Finance id="Finance"/>
             <InfoTextFinance/>
-            <Healthcare/>
+            <Healthcare id="Healthcare"/>
             <InfoTexthealthcare/>
-            <Education/>
+            <Education id="Education"/>
             <InfoTextEducation/>
-            <Food/>
+            <Food id="FoodService"/>
             <InfoTextFood/>
-            <SaaS/>
+            <SaaS id="Saas"/>
             <InfoTextSaaS/>
             <Footer/>
         </>
--- src/views/components/industries/HeroView.jsx
+++ src/views/components/industries/HeroView.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import IconText from "../assets/Vector.svg"
+
 function IndustriesView() {
     const heroText = "Productivity and Efficiency tools for all companies."
-    const videoUrl = "https://s3-figma-videos-production-sig.figma.com/video/835212680197847456/TEAM/961f/0f7b/-419e-460b-87ae-f4cab050476e?Expires=1695600000&Signature=mqfK8E5sPt8I32L19Ll4sr-Crgg3kvW1XKHQoJ-SrttW1RNdOk9a~SCSUN9Zj8zHpYZROXnQFFi3pFub9UEN0onI4CU1OBnAYkM7xUiQNNLKj0jw80QqrOLLc3AyObPZm4vpx68eU6QD6gczVMO~Gtbvug723oHTdInwbnKNa5HiXJNtWnORDYBnFGEWmmaD5GyTU0nraqJm3OgilQwSH541zvD5haqei~X0zg9MAygKe88y3bKGZSP8FcpDEEJmUzBUPoO7S1~TdbpfG-dIsSBzHI-g-b-BmT0CMefN86cCGGMWAOQzDDb4zvBFjX0YDEmyEEZ88DHJytdEZ9fZ-A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
     return (
         <>
             <div className="hero-industries">
                 <div className="redirect-vide">go</div>
-                <div className="go-to">setinha e texto estranho</div>
+                <a className="go-to" href="https://youtu.be/u7dtZ9tL_k4?si=rSGvrdI0ekJWlkSC" target="_black" rel="noopener noreferrer " ><img src={IconText}></img></a>
                 <div className="content-center">
                     <h1>{heroText}</h1>
                 </div>
