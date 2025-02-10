1a1e1b735ae0db241f05b981384eff914623d8b7
alanny-design <alannylucena@gmail.com>
2025-02-10 09:31:09 -0300

    newvideo

--- src/sections/hero/hero.jsx
+++ src/sections/hero/hero.jsx
@@ -2,13 +2,12 @@ import React, { Component } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { LogoAi, LogoDev, LogoUx, LogoXr } from "./logos";
 import "./hero.scss";
-import VideoPlay from "./videoPlay.tsx";
 
 export class Hero extends Component {
   render() {
     return (
-      <Container  className="mx-auto heroComp mt-5 pt-5">
-        <Row  >
+      <Container className="mx-auto heroComp mt-5 pt-5">
+        <Row>
           <Col className="logosX">
             <LogoAi />
             <LogoDev />
@@ -18,11 +17,23 @@ export class Hero extends Component {
         </Row>
         <Row className="m-auto">
           <Col>
-            <h1>Power your Business Success, Explore the Future with our Resource Suite</h1>
+            <h1>
+              Power your Business Success, Explore the Future with our Resource
+              Suite
+            </h1>
           </Col>
         </Row>
-        <Col>
-          <VideoPlay />
+        <Col className="mx-auto video mt-1 pt-1">
+          <iframe
+            width="860"
+            height="415"
+            src="https://www.youtube.com/embed/wby-PzPEXGQ?si=DlfJLiMraALI_T0i&amp;controls=0"
+            title="YouTube video player"
+            frameborder="20"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </Col>
       </Container>
     );
